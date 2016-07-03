54e9ecd166eddb63e21cf744031964449f58a1ad
Wolfgang Steiner <wolfgang.steiner@gmail.com>
2016-07-04 00:00:55 +0200

    categories established

--- app/src/main/java/com/example/android/tourguide/MainActivity.java
+++ app/src/main/java/com/example/android/tourguide/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.android.tourguide;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.view.View.OnClickListener;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        TextView parks = (TextView) findViewById(R.id.parks_button);
+        parks.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                startActivity(new Intent(MainActivity.this, ParksActivity.class));
+            }
+        });
+
+        TextView monumentsButton = (TextView) findViewById(R.id.monuments_button);
+        monumentsButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                startActivity(new Intent(MainActivity.this, MonumentsActivity.class));
+            }
+        });
+
+        TextView museumsButton = (TextView) findViewById(R.id.museums_button);
+        museumsButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                startActivity(new Intent(MainActivity.this, MuseumsActivity.class));
+            }
+        });
+
+        TextView sightsButton = (TextView) findViewById(R.id.sights_button);
+        sightsButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                startActivity( new Intent(MainActivity.this, SightsActivity.class));
+            }
+        });    }
 }
